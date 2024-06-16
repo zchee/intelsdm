380f8066ee10325999c70d00bae38a7066215156
Koichi Shiraishi <zchee.io@gmail.com>
2024-06-16 10:00:52 +0900

    intelsdm: add package top-level synopsis
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ intelsdm.go
@@ -0,0 +1,5 @@
+// Copyright 2024 The intelsdm Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package intelsdm provides the parses Intel® 64 and IA-32 Architectures Software Developer’s Manuals.
+package intelsdm
